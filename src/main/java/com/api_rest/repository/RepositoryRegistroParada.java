4f92ccfbc553a57bd6f9157abf6aef0a43d90f3c
joasmarquesoliveira <joas.oliveira@germinare.org.br>
2025-10-28 23:39:31 -0300

    Ajustando as mudanças do banco SQL popularizado

--- src/main/java/com/api_rest/repository/RepositoryRegistroParada.java
+++ src/main/java/com/api_rest/repository/RepositoryRegistroParada.java
@@ -11,12 +11,11 @@ import java.sql.Time;
 
 public interface RepositoryRegistroParada extends JpaRepository<RegistroParadas, Long> {
 
-    @Query(value = "SELECT func_inserir_registro_parada(:idMaquina, :idUsuario, :idManutencao, :tipoParada, :horaInicio, :horaFim, :date, :descricao)",
+    @Query(value = "SELECT func_inserir_registro_parada(:idMaquina, :idUsuario, :idManutencao, :horaInicio, :horaFim, :date, :descricao)",
             nativeQuery = true)
     Integer inserirRegistroParada(@Param("idMaquina") Integer idMaquina,
                                   @Param("idUsuario") Integer idUsuario,
                                   @Param("idManutencao") Integer idManutencao,
-                                  @Param("tipoParada") String tipoParada,
                                   @Param("horaInicio") Time horaInicio,
                                   @Param("horaFim") Time horaFim,
                                   @Param("date") Date date,
@@ -26,7 +25,6 @@ public interface RepositoryRegistroParada extends JpaRepository<RegistroParadas,
     void registrarParada(@Param("p_id_maquina") Integer idMaquina,
                          @Param("p_id_usuario") Integer idUsuario,
                          @Param("p_id_manutencao") Integer idManutencao,
-                         @Param("p_tipo_parada") String tipoParada,
                          @Param("p_hora_inicio") Time horaInicio,
                          @Param("p_hora_fim") Time horaFim,
                          @Param("p_date") Date date,
